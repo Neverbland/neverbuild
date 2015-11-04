a4bac4e45087d625f8f4542bac81a17d60638447
Kostas Vasileiou <hello@vsl.io>
2015-11-04 14:02:18 +0000

    Switched to libsass, bumped yeoman (and deps) versions, removed font-size mixin, reverted back to 100% font size.

--- app/index.js
+++ app/index.js
@@ -3,47 +3,35 @@
 var path = require('path');
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
+var mkdirp = require('mkdirp');
+var _s = require('underscore.string');
 
 var NEVERGenerator = yeoman.generators.Base.extend({
     init: function() {
         this.pkg = require('../package.json');
     },
 
-    askFor: function() {
-        var done = this.async();
-        var yosay = require('yosay');
-
-        this.log(yosay('Ey, Lad. I\'m here to help you kickstart your new NB project. Grunt, Bourbon and Neat are included.'));
-
-        var prompts = [{
-            type: 'confirm',
-            name: 'sassOption',
-            message: 'Would you like to install the SASS gem?',
-            default: false
-        }];
-
-        this.prompt(prompts, function(props) {
-            this.sassOption = props.sassOption;
-
-            done();
-        }.bind(this));
-    },
-
     app: function() {
+        this.slugName = _s.slugify(this.appname);
+
         this.imagesFolder = 'images';
         this.scssFolder = 'scss';
         this.cssFolder = 'css';
-        this.jsFolder = 'javascripts';
+        this.jsFolder = 'javascript';
+
+        this.log(chalk.magenta(
+            '[ Creating the structure. ]' +
+            ' '
+        ));
 
         // Creating all the project folders
-        this.mkdir(this.imagesFolder);
-        this.mkdir(this.scssFolder);
-        this.mkdir(this.cssFolder);
-        this.mkdir(this.jsFolder);
+        mkdirp(this.imagesFolder);
+        mkdirp(this.scssFolder);
+        mkdirp(this.cssFolder);
+        mkdirp(this.jsFolder);
 
         // Moving the right files/folders to the right folders
         this.template('_package.json', 'package.json');
-        this.template('_bower.json', 'bower.json');
         this.copy('_Gruntfile.js', 'Gruntfile.js');
         this.copy('_gitignore', '.gitignore');
         this.copy('_editorconfig', '.editorconfig');
@@ -57,26 +45,11 @@ var NEVERGenerator = yeoman.generators.Base.extend({
     },
 
     install: function() {
-        if (!this.options['skip-install']) {
-            if (this.sassOption) {
-                var gemSpawn;
+        this.log(chalk.green(
+            '[ Cool. ]'
+        ));
 
-                this.log(chalk.magenta('[ Installing the SASS gem ]'));
-
-                gemSpawn = this.spawnCommand('sudo', ['gem', 'install', 'sass', '-v', '3.4.13']);
-
-                gemSpawn.on('close', function(code) {
-                    if (code) {
-                        this.log(chalk.red('Gem couldn\'t be installed! WAT! Error: ' + code));
-                    } else {
-                        this.log(chalk.green('Gem installed successfully. Wow.'));
-                        this.installDependencies();
-                    }
-                }.bind(this));
-            } else {
-                this.installDependencies();
-            }
-        }
+        this.installDependencies();
     }
 });
 
