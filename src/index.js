60d6cf7c95da92c69f7a39633739687aea52d9b9
Maxime Bonhomme <maxime@neverbland.com>
2016-10-03 18:09:41 +0100

    postcss-flexbugs-fixes, postcss-loader, promise and react-router as general dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { Router, Route, browserHistory } from 'react-router'
 
 import Shell from './js/shell'
