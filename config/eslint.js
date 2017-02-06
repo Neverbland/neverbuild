f941c1d338087c2d61aae4282ff8e207534e2184
Piers Olenski <hello@piers.io>
2017-02-06 13:11:59 +0000

    Relaxed some of the more annoying linting rules

--- config/eslint.js
+++ config/eslint.js
@@ -32,17 +32,22 @@ module.exports = {
     }
   },
   rules: {
-    'comma-dangle': ['error', 'never'],
-    'class-methods-use-this': [0],
-    'generator-star-spacing': [0],
-    'max-len': [0],
     'arrow-parens': [0],
+    'class-methods-use-this': [0],
+    'comma-dangle': ['error', 'never'],
     'global-require': [0],
+    'import/no-dynamic-require': [0],
+    'import/prefer-default-export': [0],
+    'jsx-a11y/no-static-element-interactions': [0],
+    'max-len': [0],
+    'no-plusplus': [0],
+    'radix': [0],
+    'react/forbid-prop-types': [0],
     'react/jsx-filename-extension': [1, {
       extensions: ['.js', '.jsx']
     }],
     'react/jsx-no-bind': [0],
-    'react/forbid-prop-types': [0],
+    'react/no-danger': [0],
     'react/prop-types': [0],
     'react/react-in-jsx-scope': [0],
     'react/style-prop-object': [0]
