26dcd7d8834690f2591b5de2ccc6b77b5fe8739a
Piers Olenski <hello@piers.io>
2017-02-07 12:41:06 +0000

    Client side routing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import { Router, Route, browserHistory } from 'react-router';
 
 import Shell from './js/containers/shell';
 import Home from './js/containers/home';
+import Blog from './js/containers/blog';
+import Article from './js/containers/article';
+import Contact from './js/containers/contact';
+
+require('isomorphic-fetch');
 
 // eslint-disable-next-line
 import './css/app.scss';
@@ -14,6 +19,9 @@ ReactDOM.render(
   <Router history={browserHistory}>
     <Route component={Shell}>
       <Route path="/" component={Home} />
+      <Route path="/blog" component={Blog} />
+      <Route path="blog/:guid" component={Article} />
+      <Route path="/contact" component={Contact} />
     </Route>
   </Router>,
   document.getElementById('root')
