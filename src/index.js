6a78592e74d866dd04a258db3ff4ebb5c815da2c
Adrien Grsmto <grsmto@gmail.com>
2017-07-25 16:08:10 +0100

    apply new template

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 
-import Shell from './js/containers/shell';
-import Home from './js/containers/home';
+import registerServiceWorker from './utils/registerServiceWorker';
+
+import Shell from './containers/shell';
+import Home from './containers/home';
 
 // eslint-disable-next-line
 import './css/app.scss';
-// eslint-disable-next-line
-import 'file?name=[name].[ext]!./.htaccess';
 
 ReactDOM.render(
   <Router history={browserHistory}>
@@ -18,3 +18,5 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root')
 );
+
+registerServiceWorker();
