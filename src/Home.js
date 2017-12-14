da1d7bdb094d5e620576809b9c82361fc8f4d3a5
Rafael Derolez <rafaelderolez@gmail.com>
2017-12-14 16:29:37 +0000

    Fix React import

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import styles from './Home.scss';
 
-class Home {
+class Home extends Component {
   render() {
     console.log(styles);
     return (
