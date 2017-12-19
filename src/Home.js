39e43055b8081e3f52da1e54a7f9c57c6aa90beb
Rafael Derolez <rafaelderolez@gmail.com>
2017-12-19 13:24:10 +0100

    Moving back to css-modules for now

--- src/Home.js
+++ src/Home.js
@@ -1,70 +1,32 @@
 import React, { Component } from 'react';
-import styled, { keyframes } from 'styled-components';
-import neverbland from './logo.svg';
-
-const Wrapper = styled.div`
-  text-align: center;
-`;
-
-const rotate360 = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-
-  to {
-    transform: rotate(360deg);
-  }
-`;
-
-const Header = styled.div`
-  background-color: #222;
-  height: 150px;
-  padding: 20px;
-  color: white;
-`;
-
-const Logo = styled.img`
-  animation: ${rotate360} infinite 20s linear;
-  height: 80px;
-`;
-
-const Intro = styled.div`
-  font-size: large;
-`;
-
-const Resources = styled.ul`
-  list-style: none;
-`;
-
-const Resource = styled.li`
-  display: inline-block;
-  padding: 1rem;
-`;
+import logo from './logo.svg';
+import styles from './Home.module.css';
 
 class Home extends Component {
   render() {
+    console.log(styles);
     return (
-      <Wrapper>
-        <Header>
-          <Logo src={neverbland} alt="Neverbland Logo" />
+      <div className={styles.home}>
+        <div className={styles.homeHeader}>
+          <img src={logo} className={styles.homeLogo} alt="logo" />
           <h2>Welcome to Razzle dsfasf</h2>
-        </Header>
-        <Intro>
+        </div>
+        <p className={styles.homeIntro}>
           To get started, edit <code>src/App.js</code> or{' '}
           <code>src/Home.js</code> and save to reload.
-        </Intro>
-        <Resources>
-          <Resource>
+        </p>
+        <ul className={styles.homeResources}>
+          <li>
             <a href="https://github.com/jaredpalmer/razzle">Docs</a>
-          </Resource>
-          <Resource>
+          </li>
+          <li>
             <a href="https://github.com/jaredpalmer/razzle/issues">Issues</a>
-          </Resource>
-          <Resource>
+          </li>
+          <li>
             <a href="https://palmer.chat">Community Slack</a>
-          </Resource>
-        </Resources>
-      </Wrapper>
+          </li>
+        </ul>
+      </div>
     );
   }
 }
