00b93e5292eb67e695d7331f7bb0235a0919dd7d
Rafael Derolez <rafaelderolez@gmail.com>
2017-12-20 12:25:02 +0100

    Update container and component folder structure a bit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Route from 'react-router-dom/Route';
 import Switch from 'react-router-dom/Switch';
-import Home from 'containers/Home';
+import Home from 'containers/Home/Home';
 
 const App = () => (
   <Switch>
---
+++ src/components/Intro/Intro.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Intro.scss';
+
+const Intro = () => (
+  <p className={styles.intro}>
+    To get started, edit <code>src/App.js</code> or <code>src/Home.js</code> and
+    save to reload.
+  </p>
+);
+
+export default Intro;
