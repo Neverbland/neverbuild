4cf7de90685be22216ffa69193c4bdf7b94078bb
Piers Olenski <hello@piers.io>
2017-12-20 18:43:50 +0000

    Added styles for missing page

--- src/js/containers/Missing.js
+++ src/js/containers/Missing.js
@@ -1,11 +1,11 @@
 // Dependencies
 import React from 'react';
 // Styles
-import styles from 'css/containers/home.scss';
+import styles from 'css/containers/missing.scss';
 
 const Missing = () => (
-  <div className={styles.home}>
-    <div className={styles.home__inner}>
+  <div className={styles.missing}>
+    <div className={styles.missingInner}>
       <h2>Welcome to Neverbuild</h2>
       <p>some stuff</p>
     </div>
