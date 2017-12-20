884ab36f1bc58f52a1b106f89c796382be1c7f1e
Piers Olenski <hello@piers.io>
2017-12-20 16:42:00 +0000

    Remove Muli

--- src/server.js
+++ src/server.js
@@ -45,7 +45,6 @@ server
       assets.client.js
     }" defer crossorigin></script>`
 }
-            <link href="https://fonts.google.com/specimen/Muli?selection.family=Muli:400,600,700i,900" rel="stylesheet">
           </head>
           <body>
             ${svgSprite.stringify()}
