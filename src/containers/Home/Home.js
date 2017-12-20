3db0d02cea0e0b1e82360df332e84e0894f27637
Piers Olenski <hello@piers.io>
2017-12-20 12:13:21 +0000

    Added example of SVG component at work

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,18 +1,19 @@
+// Dependencies
 import React from 'react';
-
 // Components
 import Intro from 'components/Intro/Intro';
-
+import Icon from 'components/Icon/Icon';
 // Styles
 import styles from './Home.scss';
-
 // Assets
-import logo from './logo.svg';
+import logo from '../../images/logo.svg';
+import spriteLogo from '../../images/sprite/spriteLogo.svg';
 
 const Home = () => (
   <div className={styles.home}>
     <div className={styles.homeHeader}>
       <img src={logo} className={styles.homeLogo} alt="logo" />
+      <Icon glyph={spriteLogo} />
       <h2>Welcome to Neverbuild</h2>
     </div>
     <Intro />
