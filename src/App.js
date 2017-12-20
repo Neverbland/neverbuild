68b076f770913d350c687104cba712d2a8a50fbb
Rafael Derolez <rafaelderolez@gmail.com>
2017-12-20 12:36:21 +0100

    Added base styles from previous neverbuild.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Route from 'react-router-dom/Route';
 import Switch from 'react-router-dom/Switch';
 import Home from 'containers/Home/Home';
+import 'styles/app.scss';
 
 const App = () => (
   <Switch>
