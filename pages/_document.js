52643fd092ae187c236714a2769dcc355ae737b3
Piers Olenski <hello@piers.io>
2018-07-03 15:48:15 +0100

    Add shrink-to-fit=no

--- pages/_document.js
+++ pages/_document.js
@@ -22,7 +22,7 @@ export default class MyDocument extends Document {
         />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {/* https://css-tricks.com/essential-meta-tags-social-media/ */}
         <meta
