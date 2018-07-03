08b278c735f4effa445f1c11ebb95f86f7b84b50
Piers Olenski <hello@piers.io>
2018-07-03 13:07:20 +0100

    A Fresh Start

--- src/js/services/cms.js
+++
@@ -1,9 +0,0 @@
-export default function (query) {
-  return fetch('https://api.graphcms.com/simple/v1/neverbuild', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ query })
-  })
-    .then(res => res.json())
-    .then(res => ({ data: res.data }));
-}
