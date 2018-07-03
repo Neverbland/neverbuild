e9108c1db6b648dbdcf5cb238b6f39fb4776f343
Piers Olenski <hello@piers.io>
2018-07-03 15:19:20 +0100

    Add max-props-per-line

--- .eslintrc.js
+++ .eslintrc.js
@@ -66,6 +66,7 @@ module.exports = {
       }
     ],
     'react/jsx-no-bind': [0],
+    'react/jsx-max-props-per-line': [1, { maximum: 1 }],
     'react/no-danger': [0],
     'react/prop-types': [0],
     'react/react-in-jsx-scope': [0],
