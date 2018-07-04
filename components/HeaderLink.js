984bcf89dd7e991726368cfaf0a8b43312a90c76
Piers Olenski <hello@piers.io>
2018-07-04 19:53:07 +0100

    HeaderLink component

---
+++ components/HeaderLink.js
@@ -0,0 +1,13 @@
+// Components
+import Link from 'next/link';
+
+const HeaderLink = ({ name, href }) => (
+  <Link
+    href={href}
+    prefetch
+  >
+    <a>{name}</a>
+  </Link>
+);
+
+export default HeaderLink;
