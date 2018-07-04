3cca3ae7ee02086da1b826b7829ac996585cb460
Piers Olenski <hello@piers.io>
2018-07-04 20:49:54 +0100

    Add site wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import App, { Container } from 'next/app';
 import React from 'react';
+import styled from 'styled-components';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+
+const SiteWrapper = styled.div`
+  height: 100vh;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  grid-row-gap: 24px;
+  grid-template-columns:
+    [edge-start] minmax(16px, 1fr) [content-start] minmax(0, 1200px)
+    [content-end] minmax(16px, 1fr) [edge-end];
+`;
 
 export default class MyApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
@@ -18,9 +29,11 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props;
     return (
       <Container>
-        <Header />
-        <Component {...pageProps} />
-        <Footer />
+        <SiteWrapper>
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+        </SiteWrapper>
       </Container>
     );
   }
