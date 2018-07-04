ef0abf666e93421522035b73cf6271f3c69d9085
Piers Olenski <hello@piers.io>
2018-07-04 20:49:41 +0100

    Add Placeholder component

---
+++ components/Placeholder.js
@@ -0,0 +1,14 @@
+// Components
+import styled from 'styled-components';
+
+const Placeholder = styled.div`
+  background: #eee;
+  min-height: 200px;
+  grid-column: 1 / -1;
+  @media (min-width: 800px) {
+    grid-column: ${props => props.columns};
+    grid-row: ${props => props.rows};
+  }
+`;
+
+export default Placeholder;
