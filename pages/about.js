852bc9e8d3321286cee242941b656642108a819e
Piers Olenski <hello@piers.io>
2018-07-04 20:50:14 +0100

    Implement Placeholders

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,36 @@
 import Head from '../components/Head';
 import styled from 'styled-components';
+import Placeholder from '../components/Placeholder';
 
 const Main = styled.main`
   grid-column: content;
+  display: grid;
+  grid-template-columns: repeat(12, minmax(0, 1fr));
+  grid-gap: 24px;
 `;
 
 export default () => (
   <Main>
     <Head title="About" />
-    {'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'.repeat(10)}
+    <Placeholder
+      columns="1 / 9"
+      rows="1 / 2"
+    />
+    <Placeholder
+      columns="9 / 13"
+      rows="1 / 4"
+    />
+    <Placeholder
+      columns="1 / 9"
+      rows="2 / 3"
+    />
+    <Placeholder
+      columns="1 / 9"
+      rows="3 / 4"
+    />
+    <Placeholder
+      columns="1 / 9"
+      rows="4 / 5"
+    />
   </Main>
 );
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
 import styled from 'styled-components';
 import Head from '../components/Head';
+import Placeholder from '../components/Placeholder';
 
 const Main = styled.main`
   grid-column: content;
+  display: grid;
+  grid-template-columns: repeat(12, minmax(0, 1fr));
+  grid-gap: 24px;
 `;
 
 export default () => (
   <Main>
     <Head />
-    {'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'.repeat(10)}
+    {[...Array(12)].map(i => (<Placeholder
+      key={i}
+      columns="span 4"
+    />))}
   </Main>
 );
