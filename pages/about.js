3c9fef53bd67076a18d752c23d4bc9de12019c00
Piers Olenski <hello@piers.io>
2018-07-05 12:39:33 +0100

    Remove unused import

--- pages/about.js
+++ pages/about.js
@@ -1,30 +1,14 @@
 import Head from '../components/Head';
-import styled from 'styled-components';
 import Main from '../components/Main';
 import Placeholder from '../components/Placeholder';
 
 export default () => (
   <Main>
     <Head title="About" />
-    <Placeholder
-      columns="1 / 9"
-      rows="1 / 2"
-    />
-    <Placeholder
-      columns="9 / 13"
-      rows="1 / 4"
-    />
-    <Placeholder
-      columns="1 / 9"
-      rows="2 / 3"
-    />
-    <Placeholder
-      columns="1 / 9"
-      rows="3 / 4"
-    />
-    <Placeholder
-      columns="1 / 9"
-      rows="4 / 5"
-    />
+    <Placeholder columns="1 / 9" rows="1 / 2" />
+    <Placeholder columns="9 / 13" rows="1 / 4" />
+    <Placeholder columns="1 / 9" rows="2 / 3" />
+    <Placeholder columns="1 / 9" rows="3 / 4" />
+    <Placeholder columns="1 / 9" rows="4 / 5" />
   </Main>
 );
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import styled from 'styled-components';
 import Head from '../components/Head';
 import Main from '../components/Main';
 import Placeholder from '../components/Placeholder';
@@ -6,9 +5,6 @@ import Placeholder from '../components/Placeholder';
 export default () => (
   <Main>
     <Head />
-    {[...Array(12)].map(i => (<Placeholder
-      key={i}
-      columns="span 4"
-    />))}
+    {[...Array(12)].map(i => <Placeholder key={i} columns="span 4" />)}
   </Main>
 );
