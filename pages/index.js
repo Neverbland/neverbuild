52f19a1ed6315308e6dacc3dac469d45c45ff40e
Piers Olenski <hello@piers.io>
2018-07-05 14:38:33 +0100

    Give Placeholder a proper unique key

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,6 @@ import Placeholder from '../components/Placeholder';
 export default () => (
   <Main>
     <Head />
-    {[...Array(12)].map(i => <Placeholder key={i} columns="span 4" />)}
+    {[...Array(12)].map((_, i) => <Placeholder key={i} columns="span 4" />)}
   </Main>
 );
