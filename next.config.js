b9720f7f2001a6e88107476f0538420ef542d035
Piers Olenski <hello@piers.io>
2018-07-18 18:03:22 +0100

    Added linting to the terminal

--- next.config.js
+++ next.config.js
@@ -1,18 +1,34 @@
 module.exports = {
   webpack: config => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: 'svg-sprite-loader',
-          options: {
-            includePaths: ['/static/images/sprite'],
-            symbolId: '[name]_[hash]',
-            esModule: false
+    config.module.rules.push(
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-sprite-loader',
+            options: {
+              includePaths: ['/static/images/sprite'],
+              symbolId: '[name]_[hash]',
+              esModule: false
+            }
           }
-        }
-      ]
-    });
+        ]
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'eslint-loader',
+            options: {
+              emitWarning: true,
+              configFile: '.eslintrc.js'
+            }
+          }
+        ]
+      }
+    );
     return config;
   }
 };
